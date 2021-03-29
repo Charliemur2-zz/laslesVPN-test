b5a04739621c219dcc46c49c33060279f6d5eb87
Charliemur2 <charliemurciamunoz@gmail.com>
2021-03-28 23:34:48 -0500

    create: people-testimonials secttion in all sizes

--- scripts/script.js
+++ scripts/script.js
@@ -85,9 +85,44 @@ $(function () {
           1024:{ items:1 }
       }
     })
-  } // Here, continue with the conditional to choose 
-
+  } 
 });
+/* inicializate glider */
+window.addEventListener('load', function() {
+  new Glider(document.querySelector('.glider'), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 'auto',
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  });
+})
 
 /* get vurrent year */
 window.addEventListener('load', (
